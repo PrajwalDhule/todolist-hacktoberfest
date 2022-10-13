cb59335b2b929a51c4b2891d4956d0b8d98232f3
Prajwal <prajwaldhule36@gmail.com>
2022-10-13 20:05:31 +0530

    added deadline functionality

--- controllers/item.js
+++ controllers/item.js
@@ -1,18 +1,20 @@
-const _ = require('lodash')
-const Item = require('../models/item')
-const List = require('../models/list')
+const _ = require("lodash");
+const Item = require("../models/item");
+const List = require("../models/list");
 
 const item1 = new Item({
-  name: 'Welcome to your ToDo List',
-})
+  name: "Welcome to your ToDo List",
+  deadline: "10:00",
+});
 const item2 = new Item({
-  name: ' <-- Hit this box to delete this task',
-})
+  name: " <-- Hit this box to delete this task",
+  deadline: "12:00",
+});
 const item3 = new Item({
-  name: 'Hit + button to add a new task',
-})
-
-const defaultItems = [item1, item2, item3]
+  name: "Hit + button to add a new task",
+  deadline: "15:00",
+});
+const defaultItems = [item1, item2, item3];
 
 exports.getAllItems = (req, res) => {
   // rendering database items and passing it to home page
@@ -22,79 +24,81 @@ exports.getAllItems = (req, res) => {
       // add list items to database
       Item.insertMany(defaultItems, (err) => {
         if (err) {
-          console.log(err)
+          console.log(err);
         } else {
-          console.log('successfully added default items to DB')
+          console.log("successfully added default items to DB");
         }
-      })
+      });
 
       // go to homepage
-      res.redirect('/')
+      res.redirect("/");
     } else {
       // if not empty, render with the found results...and pass them to the page
-      res.render('list', { listTitle: 'Today', results })
+      res.render("list", { listTitle: "Today", results });
     }
-  })
-}
+  });
+};
 
 // posts method for both default list and dynamic lists
 exports.postItem = (req, res) => {
   // getting the item name/content
-  let itemName = req.body.toDo
+  let itemName = req.body.toDo;
+  let itemDeadline = req.body.deadline;
 
   // getting the list title
-  let listName = req.body.list
+  let listName = req.body.list;
 
   // creating the list item
   const newItem = new Item({
     name: itemName,
-  })
+    deadline: itemDeadline
+  });
 
   // checking if the route is default or not
-  if (listName == 'Today') {
+  if (listName == "Today") {
     // saving the newly created item to the database
-    newItem.save()
+    newItem.save();
 
     // redirect to home page
-    res.redirect('/')
+    res.redirect("/");
   } else {
     // finding in the List document
     List.findOne({ name: listName }, (err, foundList) => {
       if (err) {
-        console.log(err)
+        console.log(err);
       } else {
         // pushing the newly created item
         // to the items array of that specific list
-        foundList.items.push(newItem)
+        foundList.items.push(newItem);
         // saving to database
-        foundList.save()
+        foundList.save();
 
         // redrecting to the dynamic/specific list
-        res.redirect('/' + listName)
+        res.redirect("/" + listName);
       }
-    })
+    });
   }
-}
+};
 
 // todolist items delete post route
 exports.deleteItem = (req, res) => {
-  const checkedItemId = req.body.checkbox
+  const checkedItemId = req.body.checkbox;
 
-  const listName = req.body.listName
+  const listName = req.body.listName;
 
-  console.log(listName)
+  console.log(listName);
 
   // checking if the list from the default list
-  if (listName == 'Today') {
+  if (listName == "Today") {
     Item.findByIdAndRemove(checkedItemId, (err) => {
       if (err) {
-        console.log(err)
+        console.log(err);
       } else {
-        console.log('successfully deleted the item')
+        console.log("successfully deleted the item");
 
-        res.redirect('/')
+        res.redirect("/");
       }
-    })
+    });
   } else {
     // we have to go through list document, and search all lists
     //  then search the items array inside it and delete the list item
@@ -110,43 +114,43 @@ exports.deleteItem = (req, res) => {
       { $pull: { items: { _id: checkedItemId } } },
       (err, results) => {
         if (err) {
-          console.log(err)
+          console.log(err);
         } else {
-          res.redirect('/' + listName)
+          res.redirect("/" + listName);
         }
       }
-    )
+    );
   }
-}
+};
 
 // dynamic todo lists
 exports.getList = (req, res) => {
   // getting the list title
   // using lodash.capitalize([string])
   // to convert each string's first character to upper case and rest characters to lower case
-  const customListName = _.capitalize(req.params.id)
+  const customListName = _.capitalize(req.params.id);
 
   // finding if the dynamic list already exists or not
   List.findOne({ name: customListName }, (err, results) => {
     if (err) {
-      console.log(err)
+      console.log(err);
     } else {
       if (!results) {
         const list = new List({
           name: customListName,
           items: defaultItems,
-        })
+        });
 
-        list.save()
+        list.save();
 
-        res.redirect('/' + customListName)
+        res.redirect("/" + customListName);
 
-        console.log('does not exists')
+        console.log("does not exists");
       } else {
-        res.render('list', { listTitle: results.name, results: results.items })
+        res.render("list", { listTitle: results.name, results: results.items });
 
-        console.log('exists')
+        console.log("exists");
       }
     }
-  })
-}
+  });
+};
--- models/item.js
+++ models/item.js
@@ -1,10 +1,14 @@
-const mongoose = require('mongoose')
+const mongoose = require("mongoose");
 
 const itemsSchema = new mongoose.Schema({
   name: {
     type: String,
     required: true,
   },
-})
+  deadline: {
+    type: String,
+    default: ""
+  },
+});
 
-module.exports = mongoose.model('Item', itemsSchema)
+module.exports = mongoose.model("Item", itemsSchema);
